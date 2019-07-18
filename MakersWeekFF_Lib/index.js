c1a8fd849e5eaaef03ecb49ef905d21f92ff3d23
Reiniel Ravelo <reinielravelo@gmail.com>
2019-07-18 10:48:13 -0400

    New Branch

--- MakersWeekFF_Lib/index.js
+++ MakersWeekFF_Lib/index.js
@@ -5,8 +5,10 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
+
 
 const server = http.createServer((req, res) => {
     
@@ -75,3 +77,6 @@ function replaceTemplate(originalHtml, laptop) {
     output = output.replace(/{%ID%}/g, laptop.id);
     return output;
 }
+
+
+
